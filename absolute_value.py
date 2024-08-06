6337436f9ca630fd3427e279b96d6518bdb48df8
yuchienchen <yuchienc@yahoo.com>
2024-08-06 17:46:15 -0500

    call built-in abs function

--- absolute_value.py
+++ absolute_value.py
@@ -1,17 +1,17 @@
 def main():
     nums = [3, 2, -1, 7, -7, 5, 14]
 
-    abs_nums = []
+    # abs_nums = []
     for elem in nums:
         if elem < 0:
-            abs(elem) == - elem
-        else:
-            abs(elem) == elem
-        print(abs(elem))
-        abs_nums.append(abs(elem))
-    print(abs_nums)
-    abs_nums.sort()
-    print(abs_nums)
+            print(abs(elem))
+    #     else:
+    #         abs(elem) == elem
+    #     print(abs(elem))
+    #     abs_nums.append(abs(elem))
+    # print(abs_nums)
+    # abs_nums.sort()
+    # print(abs_nums)
     
 
 
