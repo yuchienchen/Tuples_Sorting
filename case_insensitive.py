ce6a79609d98cf3a9286039febb26bf0ec93437a
yuchienchen <yuchienc@yahoo.com>
2024-08-08 18:40:30 -0500

    Done!

--- case_insensitive.py
+++ case_insensitive.py
@@ -1,15 +1,24 @@
 def main():
     lst = ['Mehran', 'juliette', 'KaTiE', 'CS106A', 'pYtHoN']
 
+    # 1. Create a list of tuples where each tuple contains all uppercase and the original strings.
+    # 2. Sort the list of tuples based on the first element (uppercase)
     tuples_lst = []
     for elem in lst:
         upper_case = elem.upper()
-        print(upper_case)
+        # print(upper_case)
         tuples = upper_case, elem
         print(tuples)
         tuples_lst.append(tuples)
     print(tuples_lst)
+    tuples_lst.sort()
 
+    print(tuples_lst)
+    
+    # Extract the sorted original strings from the sorted list of tuples
+    sorted_lst = [t[1] for t in tuples_lst]
+
+    print(sorted_lst)
 
 
 
