46d63f99813119edc8d83822eb4e2d9647c31c3a
yuchienchen <yuchienc@yahoo.com>
2024-08-08 17:57:11 -0500

    Done!

--- absolute_value.py
+++ absolute_value.py
@@ -1,20 +1,25 @@
 def main():
     nums = [3, 2, -1, 7, -7, 5, 14]
 
-    abs_nums = []
+    # 1. Create a list of tuples where each tuple contains the absolute value and the original value.
+    # 2. Sort the list of tuples based on the first element (the absolute value)
+    tuples_nums = []
     for elem in nums:
         value = abs(elem)
-        value, elem = elem, value
-        print(value, elem)
-        abs_nums.append(elem)
-        abs_nums.sort()
+        tuple = value, elem
+        print(tuple)
+        tuples_nums.append(tuple)
+    print(tuples_nums)
+    tuples_nums.sort()
     
-    print(abs_nums)
+    print(tuples_nums)
+
+    # Extract the sorted original values from the sorted list of tuples
+    sorted_nums = [t[1] for t in tuples_nums]
+    
+    print(sorted_nums)
     
     
-
-
-
 
 if __name__ == '__main__':
     main()
