5697f63157bb71f9977570c94a77a8eef10240f6
yuchienchen <yuchienc@yahoo.com>
2024-08-09 00:28:14 -0500

    Done

--- address_zipcode.py
+++ address_zipcode.py
@@ -2,6 +2,18 @@ def main():
     addresses = [("115 Fifth Avenue", "New York", "NY", "10003"), ("9805 63rd Rd", "Rego Park", "NY", "11374"), ("1626 Shadybrook Dr", "Houston", "TX", "77094")]
     print(sorted(addresses))
 
+    print(sorted(addresses, reverse=True))
+
+    sorted_addresses = sorted(addresses, key=get_zipcode)
+    print(sorted_addresses)
+    
+    rev_sorted_addresses = sorted(addresses, key=get_zipcode, reverse=True)
+    print(rev_sorted_addresses)
+
+def get_zipcode(address):
+    return address[3]
+
+
 
 
 
