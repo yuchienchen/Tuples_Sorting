9ffc011170e66851290075cf6290c5612768b52c
yuchienchen <yuchienc@yahoo.com>
2024-08-09 00:20:17 -0500

    Initialize

---
+++ address_zipcode.py
@@ -0,0 +1,14 @@
+def main():
+    addresses = [("115 Fifth Avenue", "New York", "NY", "10003"), ("9805 63rd Rd", "Rego Park", "NY", "11374"), ("1626 Shadybrook Dr", "Houston", "TX", "77094")]
+    print(sorted(addresses))
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
